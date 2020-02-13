333c5dfe626723e2e66cc7c44f6678d57131307e
yujin2030 <56426460+yujin2030@users.noreply.github.com>
2020-02-13 14:29:49 +0900

    C# WCF 연산프로그램 서버
    C# WCF 연산프로그램 서버

---
+++ C# WCF 연산프로그램 클라이언트/1104 오후 실습 클라이언트/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1104_오후_실습_클라이언트.ServiceReference2;
+
+namespace _1104_오후_실습_클라이언트
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleClient r = new ExampleClient();
+
+            Cal[] arr = r.GetExample(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string str = r.PrintExample(arr[i].Num1, arr[i].Num2, arr[i].Oper);
+                Console.Write(str);
+
+                float result = float.Parse(Console.ReadLine());
+
+                if (r.InputExample(arr[i].Num1, arr[i].Num2, arr[i].Oper, result))
+                    Console.WriteLine("정답");
+                else
+                    Console.WriteLine("오답");
+            }
+        }
+    }
+}
