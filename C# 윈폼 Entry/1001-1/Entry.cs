ce00961fc4f688fcc5dab68f8fc84737edfb9b5c
yujin2030 <56426460+yujin2030@users.noreply.github.com>
2020-02-13 14:03:43 +0900

    C# 윈폼 Entry
    한 줄의 텍스트를 편집할수 있는 컨트롤

---
+++ C# 윈폼 Entry/1001-1/Entry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _1001_1
+{
+    class Entry
+    {
+        internal string Name { get; private set; }
+        internal int Tel { get; private set; }
+
+        internal static Entry MakeEntry(XmlReader xr)
+        {
+            string name = string.Empty;
+            int tel = 0;
+            xr.ReadToDescendant("name");
+            name = xr.ReadElementString("name");
+            xr.ReadToNextSibling("tel");
+            xr.ReadStartElement("tel");
+            tel = int.Parse(xr.Value);
+            return new Entry(name, tel);
+        }
+
+        public Entry(string name, int tel)
+        {
+            Name = name;
+            Tel = tel;
+        }
+
+        public override string ToString()   //참조형식 재정의
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ C# 윈폼 Entry/1001-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _1001_1
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+            ArrayList ar = new ArrayList();
+            XmlReader reader = XmlReader.Create("data.xml");
+            while(reader.Read())
+            {
+                if (reader.IsStartElement("entry"))
+                {
+                    Entry entry = Entry.MakeEntry(reader);
+                    if(entry != null)
+                    {
+                        ar.Add(entry);
+                    }
+                }
+
+            }
+            Console.WriteLine("엔트리 갯수 :{0}", ar.Count);
+            foreach(Entry entry in ar)
+            {
+                Console.WriteLine("이름:{0} 전화번호:{1}", entry.Name, entry.Tel);
+            }
+       }
+    }
+}
