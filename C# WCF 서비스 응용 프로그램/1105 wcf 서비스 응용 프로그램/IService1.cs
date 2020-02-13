49e1f7d3b1146d5accaa0f8cdc321f0b0ed96435
yujin2030 <56426460+yujin2030@users.noreply.github.com>
2020-02-13 14:51:22 +0900

    C# WCF 서비스 응용 프로그램
    C# WCF 서비스 응용 프로그램

---
+++ C# WCF 서비스 응용 프로그램/1105 wcf 서비스 응용 프로그램/IService1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace _1105_wcf_서비스_응용_프로그램
+{
+    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드 및 config 파일에서 인터페이스 이름 "IService1"을 변경할 수 있습니다.
+    [ServiceContract(CallbackContract = typeof(ICallback))]
+    public interface ICal
+    {
+
+        [OperationContract(IsOneWay = true)]
+        void Add(int num1, int num2);
+
+        [OperationContract(IsOneWay = true)]
+        void Sub(int num1, int num2);
+
+        [OperationContract(IsOneWay = true)]
+        void Mul(int num1, int num2);
+
+        [OperationContract(IsOneWay = true)]
+        void Div(int num1, int num2);
+
+        // TODO: 여기에 서비스 작업을 추가합니다.
+    }
+
+    public interface ICallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void Result(float result);
+    }
+
+
+    // 아래 샘플에 나타낸 것처럼 데이터 계약을 사용하여 복합 형식을 서비스 작업에 추가합니다.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ C# WCF 서비스 응용 프로그램/1105 wcf 서비스 응용 프로그램/Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace _1105_wcf_서비스_응용_프로그램
+{
+    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드, svc 및 config 파일에서 클래스 이름 "Service1"을 변경할 수 있습니다.
+    // 참고: 이 서비스를 테스트하기 위해 WCF 테스트 클라이언트를 시작하려면 솔루션 탐색기에서Service1.svc나 Service1.svc.cs를 선택하고 디버깅을 시작하십시오.
+    public class CCal : ICal
+    {
+        private float m_result;
+
+        private ICallback callback = null;
+
+        public CCal()
+        {
+            callback = OperationContext.Current.GetCallbackChannel<ICallback>(); 
+        }
+
+
+        public void Add(int num1, int num2)
+        {
+            m_result = num1 + num2; callback.Result(m_result);
+        }
+
+        public void Sub(int num1, int num2)
+        {
+            m_result = num1 + num2; callback.Result(m_result);
+        }
+
+        public void Mul(int num1, int num2)
+        {
+            m_result = num1 * num2; callback.Result(m_result);
+        }
+
+        public void Div(int num1, int num2)
+        {
+            m_result = (float)num1 / num2; callback.Result(m_result);
+        }
+
+    }
+}
