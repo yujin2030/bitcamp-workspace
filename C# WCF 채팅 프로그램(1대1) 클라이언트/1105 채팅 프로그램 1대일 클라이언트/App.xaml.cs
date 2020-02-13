af8a1ef392c3206c8adfe2347b30bf324bce3a77
yujin2030 <56426460+yujin2030@users.noreply.github.com>
2020-02-13 14:33:26 +0900

    C# WCF 채팅 프로그램(1대1) 클라이언트
    C# WCF 채팅 프로그램(1대1) 클라이언트

---
+++ C# WCF 채팅 프로그램(1대1) 클라이언트/1105 채팅 프로그램 1대일 클라이언트/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _1105_채팅_프로그램_1대일_클라이언트
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ C# WCF 채팅 프로그램(1대1) 클라이언트/1105 채팅 프로그램 1대일 클라이언트/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ServiceModel;
+using System.Windows;
+using _1105_채팅_프로그램_1대일_클라이언트.ServiceReference1;
+
+
+namespace _1105_채팅_프로그램_1대일_클라이언트
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window, IChatCallback
+    {
+        int idx;
+        private IChat chat;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void Window_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    //2 ===================================================
+        //    InstanceContext site = new InstanceContext(this);
+        //    chat = new ChatClient(site);
+        //}
+
+        #region IChatCallback 인터페이스 함수 생성
+        public void Receive(int idx, string message)
+        {
+            string msgtemp = string.Format("{0}", message);
+            chatlist.Items.Add(msgtemp);
+        }
+
+
+        public void UserEnter(int idx)
+        {
+            string msgtemp = string.Format("{0}님이 로그인하셨습니다.", idx);
+            chatlist.Items.Add(msgtemp);
+        }
+        #endregion
+
+        #region 로그인/로그아웃 핸들러
+        private void BtnJoin_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)btnJoin.Content == "로그인")
+                this.Connect();
+            else
+                this.DisConnect();
+
+        }
+
+        private void Connect()
+        {
+            try
+            {
+                idx = int.Parse(seatbox.Text);
+
+                //서버 접속
+                if (chat.Join(idx))
+                {
+                    MessageBox.Show("접속 성공");
+                }
+                else
+                    MessageBox.Show("접속 실패");
+
+                btnJoin.Content = "로그아웃";
+                string login = string.Format("{0}님이 로그인하셨습니다.", seatbox.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("접속 오류 : " + ex.Message);
+            }
+        }
+
+        private void DisConnect()
+        {
+            try
+            {
+                chat.Leave(idx);
+                btnJoin.Content = "로그인";
+
+                string logout = string.Format("{0}님이 로그아웃하셨습니다.", seatbox.Text);
+                chatlist.Items.Add(logout);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("나가기 오류 :{0}", ex.Message);
+            }
+        }
+        #endregion
+
+
+        // 메시지 전송
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnJoin.Content.Equals("로그아웃"))
+            {
+                string msg = msgbox.Text;
+
+                string temp = string.Format("[{0}]", msg);
+
+                chat.Say(idx, msg);
+
+                msgbox.Clear();
+            }
+            else
+                MessageBox.Show("로그인을 하시오");
+
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            //2 ===================================================
+            InstanceContext site = new InstanceContext(this);
+            chat = new ChatClient(site);
+        }
+    }
+}
