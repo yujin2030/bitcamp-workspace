ec7f5043039b96a1c0ed2ae22abc3fe7b06200e1
yujin2030 <56426460+yujin2030@users.noreply.github.com>
2020-02-13 13:27:11 +0900

    C# WPF 배경과 콤보박스 색 바꾸기
    C# WPF 배경과 콤보박스 색 바꾸기

---
+++ C# WPF 배경과 콤보박스 색 바꾸기/1028조별/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1028조별
+{
+    class Account : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int id;
+        private string name;
+        private int balance;
+        private DateTime date;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Id"));
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public int Balance
+        {
+            get { return balance; }
+            set
+            {
+                balance = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Balance"));
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Date"));
+            }
+        }
+
+        public override string ToString()
+        {
+            return Id+ " : "+Name  + "   "+ Balance + "   " + Date;
+        }
+    }
+}
---
+++ C# WPF 배경과 콤보박스 색 바꾸기/1028조별/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1028조별
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class IndexToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int idx = (int)value;
+            switch(idx)
+            {
+                case 0: return "Red";
+                case 1: return "Blue";
+                case 2: return "Yellow";
+                case 3: return "Green";
+                case 4: return "White";
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IndexToFore : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int idx = (int)value;
+            switch (idx)
+            {
+                case 0: return "Black";
+                case 1: return "Red";
+                case 2: return "Brown";
+                case 3: return "Purple";
+                case 4: return "Silver";
+   
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
