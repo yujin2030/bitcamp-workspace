185fd2354d4314e93bcb933ad837c2b483ac75ac
yujin2030 <56426460+yujin2030@users.noreply.github.com>
2020-02-13 13:33:02 +0900

    C# WPF 터널링 버블링
    C# WPF 터널링 버블링

---
+++ C# WPF 터널링 버블링/1025(WPF 앱으로/Window1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _1025_WPF_앱으로
+{
+    /// <summary>
+    /// Window1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        List<Rectangle> rlist = new List<Rectangle>();
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Point pt = e.GetPosition(canvas1); //자별에 있는거를 소스코드에서 쓰려고 Canvas 이름 부여
+
+            Rectangle rt = new Rectangle();
+            rt.Width = 50;
+            rt.Height = 50;
+            rt.Stroke = new SolidColorBrush(Colors.Blue);
+            rt.Fill = new SolidColorBrush(Colors.Yellow);
+            rt.MouseDown += Rt_MouseDown;      //모든 버튼에 마우스 다운 이벤트 있숨
+
+            Canvas.SetLeft(rt, pt.X);
+            Canvas.SetTop(rt, pt.Y);
+
+            canvas1.Children.Add(rt);
+            rlist.Add(rt);          //리스트에 보관
+
+        }
+
+        private void Rt_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle r = (Rectangle)sender;
+            r.Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(Rectangle r in rlist)
+            {
+                double d = Canvas.GetLeft(r);
+                Canvas.SetLeft(r, d + 10);
+            }
+        }
+    }
+}
---
+++ C# WPF 터널링 버블링/1025(WPF 앱으로/Window2.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _1025_WPF_앱으로
+{
+    /// <summary>
+    /// Window2.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        public string MSG { get; set; }
+
+        //터널링 이벤트
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG = "Window_터널링->";
+        }
+
+        private void Canvas_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Canvas_터널링->";
+        }
+
+        private void Label_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Label_터널링->";
+        }
+        private void StackPanel_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "StackPanel_터널링->";
+        }
+
+        private void Ellipse_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Ellipse_터널링->";
+        }
+        private void Label_PreviewMouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Label1_터널링->";
+        }
+        //=========================================================================
+
+        //버블링 이벤트
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Window_버블링->";
+            Title = MSG;
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Canvas_버블링->";
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Label_버블링->";
+        }
+
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "StackPanel_버블링->";
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Ellipse_버블링->";
+        }
+
+        private void Label_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            MSG += "Label1_버블링->";
+        }
+    }
+}
