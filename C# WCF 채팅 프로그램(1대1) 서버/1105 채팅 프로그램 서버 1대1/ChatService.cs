109f79aaa6a0230c53c856bda2872931e9a4f658
yujin2030 <56426460+yujin2030@users.noreply.github.com>
2020-02-13 14:33:05 +0900

    C# WCF 채팅 프로그램(1대1) 서버
    C# WCF 채팅 프로그램(1대1) 서버

---
+++ C# WCF 채팅 프로그램(1대1) 서버/1105 채팅 프로그램 서버 1대1/ChatService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1105_채팅_프로그램_서버_1대1
+{
+    class ChatService : IChat
+    {
+        //델리게이트 선언
+        public delegate void Chat(int idx, string msg, string type);
+        // 개인용 델리게이트
+        private Chat MyChat;
+        //전체에게 보낼 정보를 담고 있는 델리게이트
+        IChatCallback callback = null;
+
+
+        #region 1. Join(로그인하기)
+        public bool Join(int idx)
+        {
+            MyChat = new Chat(UserHandler);
+            //2. 사용자에게 보내 줄 채널을 설정한다.
+            callback = OperationContext.Current.GetCallbackChannel<IChatCallback>();
+
+            //현재 접속자 정보를 모두에게 전달
+            BroadcastMessage(idx, "", "UserEnter");
+            return true;
+        }
+        #endregion
+
+        #region 2. Say(메시지 보내기)
+        public void Say(int idx, string msg)
+        {
+            BroadcastMessage(idx, msg, "Receive");
+        }
+        #endregion
+
+        #region 3. Leave(로그아웃 하기)
+        public void Leave(int idx)
+        {
+            Chat d = null;
+            MyChat -= d;
+        }
+        #endregion
+
+        private void BroadcastMessage(int idx, string msg, string msgType)
+        {
+            MyChat.BeginInvoke(idx, msg, msgType, new AsyncCallback(EndAsync), null);
+        }
+
+        private void UserHandler(int idx, string msg, string msgType)
+        {
+            try
+            {
+                //클라이언트에게 보내기
+                switch (msgType)
+                {
+                    case "Receive":
+                        callback.Receive(idx, msg);
+                        break;
+                    case "UserEnter":
+                        callback.UserEnter(idx);
+                        break;
+                }
+            }
+            catch//에러가 발생했을 경우
+            {
+                Console.WriteLine("{0} 에러", idx);
+            }
+        }
+
+        //
+        private void EndAsync(IAsyncResult ar)
+        {
+            Chat d = null;
+            try
+            {
+                System.Runtime.Remoting.Messaging.AsyncResult asres =
+                    (System.Runtime.Remoting.Messaging.AsyncResult)ar;
+                d = ((Chat)asres.AsyncDelegate);
+                d.EndInvoke(ar);
+            }
+            catch
+            {
+            }
+        }
+
+
+
+    }
+}
---
+++ C# WCF 채팅 프로그램(1대1) 서버/1105 채팅 프로그램 서버 1대1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1105_채팅_프로그램_서버_1대1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Address
+            Uri uri = new Uri(ConfigurationManager.AppSettings["addr"]);
+            //Contract-> Setting
+            //Binding -> App.Config
+            ServiceHost host = new ServiceHost(typeof(ChatService), uri);
+
+            //exe Hosting
+            //ServiceHost host =
+            //    new ServiceHost(typeof(_1105_채팅_프로그램_서버_1대1.ChatService)
+            //        /*new Uri("http://192.168.0.47:8888/wcf/example/helloworldservice")*/);
+
+            //오픈
+            host.Open();
+            Console.WriteLine("채팅 서비스를 시작합니다. {0}", uri.ToString());
+            Console.WriteLine("http://192.168.0.47:7000/GetService");
+            Console.WriteLine("멈추시려면 엔터를 눌러주세요..");
+            Console.ReadLine();
+            //서비스
+            host.Abort();
+            host.Close();
+
+
+        }
+    }
+}
